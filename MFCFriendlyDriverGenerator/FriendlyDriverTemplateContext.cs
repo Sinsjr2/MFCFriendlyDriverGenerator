fc82125b60baebbfdc166edce9b37a3d74b1e853
Sinsjr2 <hks15sirubia@gmail.com>
2021-10-15 23:23:43 +0900

    コンストラクタを使用するかドライバーを取得するメソッドの戻り値を変更できるようにしました。

--- MFCFriendlyDriverGenerator/FriendlyDriverTemplateContext.cs
+++ MFCFriendlyDriverGenerator/FriendlyDriverTemplateContext.cs
@@ -10,6 +10,18 @@ namespace MFCFriendlyDriverGenerator {
     ///  MFCのドライバを生成するのに必要なコントロールの情報
     ///  コントロールの名前は変数に名前をつける時に使用
     ///  ドライバの型は名前空間を含んだ型名
+    ///  コンストラクターを使用するかどうかは、ドライバーのインスタンスを作るときにドライバの型で指定した
+    ///  コンストラクターを使用するかを表します。
     /// </summary>
-    public record ControlInfo(int ControlID, string ControlName, string DriverType);
+    public record ControlInfo(int ControlID, string ControlName, string DriverType, bool UsesConstructor) {
+
+        /// <summary>
+        /// ドライバーのインスタンスを初期化するコードを返します。
+        /// </summary>
+        public string CreateDriverInstanceCode(string getCode) {
+            return UsesConstructor
+                ? "new " + DriverType + "(" + getCode + ")"
+                : getCode;
+        }
+    }
 }
