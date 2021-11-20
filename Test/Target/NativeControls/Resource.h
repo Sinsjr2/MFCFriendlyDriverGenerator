b50c70edf3f0cd989cc7c8abfa32c4afc3654ccb
Sinsjr2 <hks15sirubia@gmail.com>
2021-11-20 14:46:44 +0900

    ドライバの生成でRCファイルが読めなかったので修正しました。

--- Test/Target/NativeControls/Resource.h
+++ Test/Target/NativeControls/Resource.h
@@ -1,19 +1,33 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ で生成されたインクルード ファイル。
-// NativeControls.rc で使用されます
+// NativeControls.rc で使用
 //
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDR_NativeControlsTYPE				130
-#define ID_WINDOW_MANAGER			131
+#define IDD_MAIN                        5
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_NativeControlsTYPE          130
+#define ID_WINDOW_MANAGER               131
+#define IDC_BUTTON_TEXT                 1000
+#define IDC_BUTTON_OK                   1001
+#define IDC_CHECK                       1002
+#define IDC_COMBO1                      1003
+#define IDC_COMBO                       1003
+#define IDC_EDIT                        1004
+#define IDC_RADIO1                      1005
+#define IDC_RADIO2                      1006
+#define IDC_LIST1                       1007
+#define IDC_TREE1                       1008
+#define IDC_TAB1                        1009
+#define IDC_CURRENT_TAB_TEXT            1010
+#define IDC_BUTTON_CANCEL               1011
 
-// 新しいオブジェクトの次の既定値
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
